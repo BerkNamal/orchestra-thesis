d0315c94be8c7d1a69227e101d3b3a6dcf9c313b
Berk <namalberk@gmail.com>
2019-01-28 14:37:02 +0100

    New conf file

--- build/cooja/obj/orchestra.d
+++ build/cooja/obj/orchestra.d
@@ -1,11 +1,10 @@
-build/cooja///obj/orchestra.o: ../../../os/services/orchestra/orchestra.c \
- ../../../os/contiki.h ../../../arch/platform/cooja/./contiki-conf.h \
- project-conf.h ../../../os/contiki-default-conf.h \
- ../../../os/sys/process.h ../../../os/sys/pt.h ../../../os/sys/lc.h \
- ../../../os/sys/lc-switch.h ../../../os/sys/cc.h \
- ../../../os/sys/cc-gcc.h ../../../os/sys/autostart.h \
- ../../../os/sys/process.h ../../../os/sys/timer.h \
- ../../../os/sys/clock.h ../../../os/sys/ctimer.h \
+build/cooja///obj/orchestra.o: orchestra.c ../../../os/contiki.h \
+ ../../../arch/platform/cooja/./contiki-conf.h project-conf.h \
+ ../../../os/contiki-default-conf.h ../../../os/sys/process.h \
+ ../../../os/sys/pt.h ../../../os/sys/lc.h ../../../os/sys/lc-switch.h \
+ ../../../os/sys/cc.h ../../../os/sys/cc-gcc.h \
+ ../../../os/sys/autostart.h ../../../os/sys/process.h \
+ ../../../os/sys/timer.h ../../../os/sys/clock.h ../../../os/sys/ctimer.h \
  ../../../os/sys/etimer.h ../../../os/sys/etimer.h \
  ../../../os/sys/rtimer.h ../../../os/dev/watchdog.h \
  ../../../arch/platform/cooja/./rtimer-arch.h ../../../os/sys/pt.h \
@@ -51,14 +50,13 @@ build/cooja///obj/orchestra.o: ../../../os/services/orchestra/orchestra.c \
  ../../../os/net/routing/rpl-classic/rpl-private.h \
  ../../../os/net/ipv6/uip-debug.h ../../../os/net/net-debug.h \
  ../../../os/net/ipv6/uiplib.h
-../../../os/services/orchestra/orchestra.c :
- ../../../os/contiki.h ../../../arch/platform/cooja/./contiki-conf.h :
- project-conf.h ../../../os/contiki-default-conf.h :
- ../../../os/sys/process.h ../../../os/sys/pt.h ../../../os/sys/lc.h :
- ../../../os/sys/lc-switch.h ../../../os/sys/cc.h :
- ../../../os/sys/cc-gcc.h ../../../os/sys/autostart.h :
- ../../../os/sys/process.h ../../../os/sys/timer.h :
- ../../../os/sys/clock.h ../../../os/sys/ctimer.h :
+orchestra.c ../../../os/contiki.h :
+ ../../../arch/platform/cooja/./contiki-conf.h project-conf.h :
+ ../../../os/contiki-default-conf.h ../../../os/sys/process.h :
+ ../../../os/sys/pt.h ../../../os/sys/lc.h ../../../os/sys/lc-switch.h :
+ ../../../os/sys/cc.h ../../../os/sys/cc-gcc.h :
+ ../../../os/sys/autostart.h ../../../os/sys/process.h :
+ ../../../os/sys/timer.h ../../../os/sys/clock.h ../../../os/sys/ctimer.h :
  ../../../os/sys/etimer.h ../../../os/sys/etimer.h :
  ../../../os/sys/rtimer.h ../../../os/dev/watchdog.h :
  ../../../arch/platform/cooja/./rtimer-arch.h ../../../os/sys/pt.h :
